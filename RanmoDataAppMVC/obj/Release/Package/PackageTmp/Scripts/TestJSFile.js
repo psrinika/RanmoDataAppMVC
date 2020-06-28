c95170bb8fdd6c2bc0a8fdfc1d7d7704ab915ea9
psrinika <61997947+psrinika@users.noreply.github.com>
2020-06-28 16:31:25 -0400

    after deployment issue

---
+++ RanmoDataAppMVC/obj/Release/Package/PackageTmp/Scripts/TestJSFile.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="jquery-1.10.2.js" />
+//http://localhost/RanmoDataAppMVC/Production/Edit/@Url.Action(% 22GetItems % 22,% 20 % 22Production % 22)?term=sa&itemFor=&activeStatus=-1
+
+
+$(document).ready(function () {
+
+    $("#ItemName").autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: '/RanmoDataAppMVC/Production/GetItems/")',
+                datatype: "json",
+                data:
+                {
+                    term: request.term,
+                    itemFor: $("#ItemFor").val(),
+                    activeStatus: $("#activeStatus").val()
+                },
+                success: function (data) {
+                    response($.map(data, function (val, item) {
+                        return {
+                            label: val.ItemName,
+                            value: val.ItemName,
+                            ItemId: val.ItemId
+                        }
+                    }))
+                }
+            })
+        },
+        select: function (event, ui) {
+            $("#ItemId").val(ui.item.ItemId);
+        }
+    });
+});
